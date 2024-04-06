9e6ee06d35a008ae8455a8b7a0a015b550cb7372
nick <multik73@gmail.com>
2024-04-06 12:18:55 +0300

    Resolve ts issue

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,12 +1,7 @@
-import {Vehicle} from "../../components/VehicleTable";
-import {useState} from "react";
 import {data} from "./data.ts";
 import VehicleGrid from "../../components/VehicleGrid.tsx";
 
 const HomePage = () => {
-  const [vehicleData, setVehicleData] = useState<Vehicle[]>([]);
-  //setVehicleData(data);
-
   return (
     <>
       <VehicleGrid data={data} />
