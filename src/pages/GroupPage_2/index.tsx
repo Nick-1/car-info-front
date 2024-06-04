1c858d82e72df0bb23cbed478d17e37d2538d5c7
nick <multik73@gmail.com>
2024-06-04 22:57:25 +0300

    Change mindate for old car

--- src/pages/GroupPage_2/index.tsx
+++ src/pages/GroupPage_2/index.tsx
@@ -1,25 +1,16 @@
 import TableStatistic, {TableStatisticConfig} from "../../components/TableStatistic";
-import { KYRYLO_HOST_ID } from "../HomePage/constants";
-import {apiGetStatisticByPeriodAndHostId} from "../../api/endpoints/apiGetStatisticByPeriodAndHostId.ts";
 import {apiGetStatisticByPeriodAndGroupId} from "../../api/endpoints/apiGetStatisticByPeriodAndGroupId.ts";
 
 const GroupPage_2 = () => {
-  const config_2: TableStatisticConfig = {
-    id: KYRYLO_HOST_ID,
-    apiGetDataMethod: apiGetStatisticByPeriodAndHostId,
-    minDate: '2024-06-01'
-  }
-
-  const config_3: TableStatisticConfig = {
-    id: 3,
+  const config: TableStatisticConfig = {
+    id: 2,
     apiGetDataMethod: apiGetStatisticByPeriodAndGroupId,
-    minDate: '2024-05-12'
+    minDate: '2024-06-03'
   }
 
   return (
     <>
-      <TableStatistic config={config_2}/>
-      <TableStatistic config={config_3}/>
+      <TableStatistic config={config}/>
     </>
   )
 };
